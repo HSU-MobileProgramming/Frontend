4aca5507633ee9e8b025c95f98135acc98f3c086
binniejung <ybe1027@naver.com>
2024-10-31 11:02:00 +0900

    ✨ Feat: 여행기 생성-날짜 선택 기능 추가
    날짜 선택 모달창 생성, 모달창 세부수정 추후에

--- entities/StartLog/ui/SelectDate.jsx
+++ entities/StartLog/ui/SelectDate.jsx
@@ -1,14 +1,48 @@
 import styled from "styled-components/native";
-import { Image } from "react-native";
-// image
-import calendarIcon from '../../../assets/calendar-icon.png';
+import { Image, Text, Pressable, Modal, Platform } from "react-native";
+import { Calendar } from "react-native-calendars";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-export default function SelectDate({text}) {
+import { useState } from "react";
+
+// image
+import calendarGrey from '../../../assets/calendar-grey.png';
+import calendarPurple from '../../../assets/calendar-purple.png';
+import downArrow from '../../../assets/down-arrow.png';
+
+export default function SelectDate({ text }) {
+    const [date, setDate] = useState(null);
+    const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
+
+    const handleDatePicker = () => {
+        setIsDatePickerVisible(!isDatePickerVisible);
+    }
+
     return (
-        <SelectDateBox>
-            <Image source={calendarIcon}/>
-            
-        </SelectDateBox>
+        <Pressable onPress={() => setIsDatePickerVisible(true)}>
+            <SelectDateBox>
+                <Image source={date ? calendarPurple : calendarGrey} />
+                <DateText isDate={date}>{date ? date.toLocaleDateString() : text}</DateText>
+                <Image source={downArrow}/>
+            </SelectDateBox>
+
+            {/* 날짜 선택 모달 */}
+            <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                date={date || new Date()}
+                mode="date"
+                onConfirm={(date) => {
+                    setIsDatePickerVisible(false);
+                    setDate(date);
+                }}
+                onCancel={() => {
+                    setIsDatePickerVisible(false);
+                }}
+                display={Platform.OS === 'ios' ? 'inline' : 'default'}
+            />
+
+        </Pressable>
+
     )
 }
 
@@ -26,4 +60,13 @@ height: 0px;
 shadow-opacity: 0.1;
 shadow-radius: 5px;
 padding: 13px;
+flex-direction:row;
+align-items: center;
+justify-content: space-between;
+`;
+
+const DateText = styled.Text`
+color: ${(props) => (props.isDate ? '#393939' : '#636363')};
+font-weight: 500;
+width: 90px;
 `;
