881ce5c2ce3c6d55d71e95ae3a50c216be1e2b26
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-11-05 11:21:08 +0900

    ✨ 스플래시 화면 구현 (#6)

--- entities/Splash/ui/ButtonSection.jsx
+++ entities/Splash/ui/ButtonSection.jsx
@@ -5,17 +5,29 @@ export default function ButtonSection() {
   return (
     <MainLayout>
 
-      <StandardButton text="가입하기"/>
+      <StandardButton text="E-mail로 시작하기" backgroundColor="#fff" color="#170D43" marginBottom="10px"/>
 
-      <StandardButton text="카카오로그인"/>
+      <StandardButton text="카카오톡으로 시작하기" backgroundColor="#FAE100" color="#170D43"/>
+
+      <StyledText>이미 계정이 있으신가요?</StyledText>
         
     </MainLayout>
   )
 }
 
 const MainLayout = styled.View`
-height : 14%;
+height : 20%;
 width : 100%;
-justify-content : space-between;
 align-items : center;
 `;
+
+const StyledText = styled.Text`
+color: #FFF;
+font-family: Pretendard;
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+text-decoration-line: underline;
+margin-top : 28px;
+`
--- entities/Splash/ui/Logo.jsx
+++ entities/Splash/ui/Logo.jsx
@@ -2,29 +2,42 @@ import styled from 'styled-components'
 import LOGO from '../../../assets/logo.png'
 import PUZZLE from '../../../assets/puzzle.png'
 import PUZZLE2 from '../../../assets/puzzle2.png'
+import Line from './Line'
+import { View } from 'react-native'
 export default function Logo() {
     return (
         <MainLayout>
+            <Line opacity="0"/>
+
             <BackImg source={PUZZLE}/>
             <PuzzleImg source={PUZZLE2}/>
-            <StyledImg source={LOGO}></StyledImg>
-            <StyledText>환영합니다 :)</StyledText>
-            <StyledText fontWeight="400" fontSize="12px" marginTop="10px">특별한 방식으로 당신의 여행을 기억하세요!</StyledText>
+
+            <Title>
+                <StyledImg source={LOGO}></StyledImg>
+                <StyledText>환영합니다 :)</StyledText>
+                <StyledText fontWeight="500" fontSize="14px" marginTop="10px">특별한 방식으로 당신의 여행을 기억하세요!</StyledText>
+            </Title>
+
+            <Line/>
         </MainLayout>
     )
 }
 
 const MainLayout = styled.View`
-height : 80%;
+height : 70%;
 width : 100%;
-justify-content : center;
 align-items : center;
+justify-content : space-between;
 `;
 
+const Title = styled.View`
+justify-content : center;
+align-items : center;
+`
 const StyledText = styled.Text`
 color : #fff;
-font-weight : ${({ fontWeight }) => fontWeight || '600'};
-font-size :  ${({ fontSize }) => fontSize || '20px'};
+font-weight : ${({ fontWeight }) => fontWeight || '700'};
+font-size :  ${({ fontSize }) => fontSize || '24px'};
 margin-top :  ${({ marginTop }) => marginTop || '0'};
 `;
 
