5a32f752a9202b1d4a89e32b58545dbfa58d1097
Juye0nLee <dlwndus708@naver.com>
2024-11-16 01:20:48 +0900

    ➕ 지도 라이브러리 설치

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Splash" //초기 경로 설정
+        initialRouteName="World" //초기 경로 설정
         screenOptions={{ headerShown: false, animationEnabled: false }} //모든 스크린에서 헤더를 숨김
       >
         <Stack.Screen name="Splash" component={Splash}/>
--- pages/World.jsx
+++ pages/World.jsx
@@ -1,14 +1,16 @@
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import NavigationBar from '../shared/component/NavigationBar';
 import styled from 'styled-components';
+import TitleBar from '../shared/component/TitleBar';
+import MapSection from '../entities/World/MapSection';
 export default function World() {
   return (
     <MainLayout>
-        <Text>
-            월드
-        </Text>
+      <TitleBar text="여행자님의 세계지도" />
 
-        <NavigationBar world/>
+      <MapSection/>
+
+      <NavigationBar world />
     </MainLayout>
   )
 }
