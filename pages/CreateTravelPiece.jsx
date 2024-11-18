6c08dae743d4a8f5a08c261a95413934f5ccc4a9
binniejung <ybe1027@naver.com>
2024-11-19 01:04:54 +0900

    ✨ 조각 생성에 메모Input 추가

--- pages/CreateTravelPiece.jsx
+++ pages/CreateTravelPiece.jsx
@@ -1,14 +1,18 @@
 import styled from "styled-components/native";
 import { View } from "react-native";
 import CreatePieceTop from "../entities/CreateTravelPiece/CreatePieceTop";
+import InputMemo from "../entities/CreateTravelPiece/InputMemo";
 
-export default function CreateTravelPiece({route}) {
+export default function CreateTravelPiece({ route }) {
     const { recordType, recordImage, decoImage } = route.params; // route 객체를 통해 params에 접근해서 값을 가져와야함
 
-    
+
     return (
         <MainLayout>
-            <CreatePieceTop recordType={recordType} recordImage={recordImage} decoImage={decoImage}/>
+            <CreatePieceTop recordType={recordType} recordImage={recordImage} decoImage={decoImage} />
+            {recordType === "메모" ? (
+                <InputMemo height='190px' textCount='100'/>
+            ) : (<InputMemo />)}
         </MainLayout>
     )
 }
