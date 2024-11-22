0ce7bcb626a77ff5fba3199f27057332bcca2eeb
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-11-22 13:46:05 +0900

    💄 탐색탭 ui 구현 (#21)
    * 💄 탐색탭 기본 레이아웃 구현
    
    * ⚡️ 네비게이션바에 월드탭 연결
    
    * ✨ 탐색탭 검색 결과 화면 구현

--- pages/Search.jsx
+++ pages/Search.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
-import {View} from 'react-native'
+import React, { useState } from 'react'
+import { View } from 'react-native'
 import styled from 'styled-components'
 import NavigationBar from '../shared/component/NavigationBar'
+import TitleBar from '../shared/component/TitleBar'
+import StandardButton from '../shared/component/StandardButton'
+import StandardInput from '../shared/component/StandardInput'
+import CitySection from '../entities/Search/CitySection'
+import { ScrollView } from 'react-native-gesture-handler'
+import ResultSection from '../entities/Search/ResultSection'
 export default function Search() {
+  const [isShowResult, setIsShowResult] = useState(false);
   return (
     <MainLayout>
-        <View/>
-      <NavigationBar search/> 
+        <TitleBar text="탐색" backgroundColor="#fff" color="#5C95FB" />
+
+        <StandardInput 
+          placeholder="도시 및 국가를 검색해보세요" 
+          width="348px" 
+          height="48px" 
+          opacity="1"
+          onSubmitEditing={()=>setIsShowResult(true)}
+          returnKeyType="done"
+          marginBottom="20px"
+          />
+
+        <ScrollContainer>
+        {
+          isShowResult ? 
+          <ResultSection/> : 
+          <CitySection />
+        }
+        </ScrollContainer>
+
+
+      <NavigationBar search />
     </MainLayout>
+
   )
 }
 
 
+const ScrollContainer = styled.ScrollView`
+  flex: 1;
+  width: 100%;
+`;
 const MainLayout = styled.View`
+flex : 1;
 height : 100%;
 justify-content : space-between;
+align-items : center;
 `
